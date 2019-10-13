1dc5445d14bf603fa134ee961530c28512fff7cd
Jonah Latham <jonah.latham21@gmail.com>
2019-10-13 09:34:41 -0600

    code

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -3,14 +3,30 @@ import { Link } from "react-router-dom"
 import './Header.css'
 
 export default class Header extends Component {
+    state = {
+        makeWhite: false
+    }
+
+    handleWhiteTrue = () => {
+        this.setState({
+            makeWhite: true
+        })
+    }
+
+    handleWhiteFalse = () => {
+        this.setState({
+            makeWhite: false
+        })
+    }
+
     render() {
         return (
-            <div className='Header' style={{color: `${<Link className='link' to='/discoverquiz'>Discover</Link> ? 'white' : ''}`}}>
-                <Link style={{color: `${<Link className='link' to='/discoverquiz'>Discover</Link> ? 'white' : 'black'}`}} className='link' to='/Home'>Home</Link>
-                <Link style={{color: `${<Link className='link' to='/discoverquiz'>Discover</Link> ? 'white' : 'black'}`}} className='link' to='/createquiz'>Create New</Link>
-                <Link style={{color: `${<Link className='link' to='/discoverquiz'>Discover</Link> ? 'white' : 'black'}`}} className='link' to='/discoverquiz'>Discover</Link>
-                <Link style={{color: `${<Link className='link' to='/discoverquiz'>Discover</Link> ? 'white' : 'black'}`}} className='link' to='/userprofile'>Profile</Link>
-                <Link style={{color: `${<Link className='link' to='/discoverquiz'>Discover</Link> ? 'white' : 'black'}`}} className='link' to='/quiz/reviewsubmissions/:id'>Review</Link>
+            <div className='Header' >
+                <Link onClick={this.handleWhiteFalse} style={{ color: this.state.makeWhite === true ? 'white' : 'black' }} className='link' to='/Home'>Home</Link>
+                <Link onClick={this.handleWhiteFalse} style={{ color: this.state.makeWhite === true ? 'white' : 'black' }} className='link' to='/createquiz'>Create New</Link>
+                <Link onClick={this.handleWhiteTrue} style={{ color: this.state.makeWhite === true ? 'white' : 'black' }} className='link' to='/discoverquiz'>Discover</Link>
+                <Link onClick={this.handleWhiteFalse} style={{ color: this.state.makeWhite === true ? 'white' : 'black' }} className='link' to='/userprofile'>Profile</Link>
+                <Link onClick={this.handleWhiteFalse} style={{ color: this.state.makeWhite === true ? 'white' : 'black' }} className='link' to='/quiz/reviewsubmissions/:id'>Review</Link>
             </div>
         )
     }
--- src/Views/Discover/Discover.js
+++ src/Views/Discover/Discover.js
@@ -8,22 +8,6 @@ export default class Discover extends Component {
     state = {
         quizzes: []
     }
-    handleGenreImg = () => {
-        switch (this.state.quizzes.genre_id) {
-            case 1:
-                return <img className='discoverSwitchImg' src="https://images.unsplash.com/photo-1472173148041-00294f0814a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt="history" /> //history
-            case 2:
-                return <img className='discoverSwitchImg' src="https://images.unsplash.com/photo-1532094349884-543bc11b234d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt="science" /> //science
-            case 3:
-                return <img className='discoverSwitchImg' src="https://images.unsplash.com/photo-1509869175650-a1d97972541a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt="math" /> //math
-            case 4:
-                return <img className='discoverSwitchImg' src="https://images.unsplash.com/photo-1569701813229-33284b643e3c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1988&q=80" alt="pop culture" /> //Pop_Culture
-            case 5:
-                return <img className='discoverSwitchImg' src="https://images.unsplash.com/reserve/EnF7DhHROS8OMEp2pCkx_Dufer%20food%20overhead%20hig%20res.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1957&q=80" alt="culinary" /> //Culinary
-            case 6:
-                return <img className='discoverSwitchImg' src="https://images.unsplash.com/photo-1517562652858-8d863a9e0931?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1302&q=80" alt="general" /> //General
-        }
-    }
 
     componentDidMount() {
         axios.get('/api/quiz')
@@ -32,7 +16,6 @@ export default class Discover extends Component {
                     this.setState({
                         quizzes: response.data.quiz
                     })
-                    this.handleGenreImg()
                 } else {
                     this.props.history.push('/')
                 }
@@ -44,10 +27,10 @@ export default class Discover extends Component {
         let allQuizzes = this.state.quizzes.map((e, i) => {
             return (
                 <Link className='link' to='`/discoverquiz/:${e.id}`'>
-                <div className='quizzesDisplayed' key={e.id}>
+                <div style={{ background: e.genre_id===1 ? 'green' : e.genre_id===2 ? 'blue' : e.genre_id===3 ? 'red' : e.genre_id===4 ? 'yellow' : e.genre_id===5 ? 'orange' : 'white', color: e.genre_id===6 ? 'black' : 'white'}} className='quizzesDisplayed' key={e.id}>
                         {e.name}
                         <br />
-                        {e.genre_id}
+                        <small className='discoverSmall'>{e.genre_id===1 ? 'History' : e.genre_id===2 ? 'Science' : e.genre_id===3 ? 'Math' : e.genre_id===4 ? 'Pop Culture' : e.genre_id===5 ? 'Culinary' : 'Misc'}</small>
                     </div>
                 </Link>
             )
